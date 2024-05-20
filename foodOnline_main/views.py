2d66752c9abcfdc6f9c7b15531caba728af391a7
namnam2303 <imsofh@gmail.com>
2024-05-21 02:23:11 +0700

    now gitignore is working

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello  world!')
+    return HttpResponse('<h1>Hello world</h1>')
